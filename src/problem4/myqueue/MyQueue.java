d0fafbbb761511a2470e174e13f7a5a5eb32f04e
Sanskar <sanskar.agrawal_ccv18@gla.ac.in>
2020-03-28 17:08:46 +0530

    Creating data elements

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -6,6 +6,16 @@
  */
 package problem4.myqueue;
 // to create queue to store pre - order successor
+import com.sun.source.tree.Tree;
+import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
+import problem4.element.Element;
 
-public class MyQueue {
+import java.util.LinkedList;
+import java.util.List;
+public class MyQueue<T> {
+    private Element front;
+    private Element back;
+    private MyBinarySearchTree tree;
+
 }
