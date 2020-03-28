20e55bf81f28beea57926ca733a9d20839faa03e
Sanskar <sanskar.agrawal_ccv18@gla.ac.in>
2020-03-28 16:50:02 +0530

    Creating constructor in Student class

--- src/problem3/main/Student.java
+++ src/problem3/main/Student.java
@@ -4,5 +4,9 @@ public class Student {
     private String firstName;
     private String lastName;
     private int roll;
-
+    public Student(String firstName, String lastName, int roll) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.roll = roll;
+    }
 }
