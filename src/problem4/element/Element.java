ccfc23a631ebb00365688f959b98bab98aa240b6
Sanskar <sanskar.agrawal_ccv18@gla.ac.in>
2020-03-28 17:05:43 +0530

    Creating getter and setter

--- src/problem4/element/Element.java
+++ src/problem4/element/Element.java
@@ -8,4 +8,27 @@ public class Element<T> {
         this.node = node;
         this.preOrderSuccessor = preOrderSuccessor;
     }
+    public T getNode() {
+        return node;
+    }
+
+    public void setNode(T node) {
+        this.node = node;
+    }
+
+    public T getPreOrderSuccessor() {
+        return preOrderSuccessor;
+    }
+
+    public void setPreOrderSuccessor(T preOrderSuccessor) {
+        this.preOrderSuccessor = preOrderSuccessor;
+    }
+
+    public Element getNext() {
+        return next;
+    }
+
+    public void setNext(Element next) {
+        this.next = next;
+    }
 }
