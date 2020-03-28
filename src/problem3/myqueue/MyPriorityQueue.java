9098a680724cd0fd0c2be35bd96bb7172aed2f7d
Sanskar <sanskar.agrawal_ccv18@gla.ac.in>
2020-03-28 16:43:55 +0530

    Creating enQueue function for queue

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -5,6 +5,32 @@
  *  Time: 7:15 PM
  */
 package problem3.myqueue;
+import problem3.node.Node;
 
+import java.util.NoSuchElementException;
 public class MyPriorityQueue {
+    private Node front;
+    private Node back;
+
+    public <T>void enQueue(T data,int priority){
+        Node node = new Node(data,priority);
+        if(isEmpty() || node.getPriority() < front.getPriority()){
+            node.setNext(front);
+            front = node;
+        }
+        else {
+            Node previous = null;
+            Node current = front;
+            while(current != null && current.getPriority() <=  node.getPriority()){
+                previous = current;
+                current = current.getNext();
+            }
+            node.setNext(previous.getNext());
+            previous.setNext(node);
+        }
+    }
+    public boolean isEmpty(){
+        return front == null;
+    }
+
 }
