f4c92dce2a7026e73902a386382f202ccd5b4ed9
Sanskar <sanskar.agrawal_ccv18@gla.ac.in>
2020-03-28 17:05:05 +0530

    Creating constructor

--- src/problem4/element/Element.java
+++ src/problem4/element/Element.java
@@ -4,4 +4,8 @@ public class Element<T> {
     private T node;
     private T preOrderSuccessor;
     private Element next;
+    public Element(T node, T preOrderSuccessor) {
+        this.node = node;
+        this.preOrderSuccessor = preOrderSuccessor;
+    }
 }
