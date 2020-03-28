91243b36837aa179a1324bfa043730e3800296ff
Sanskar <sanskar.agrawal_ccv18@gla.ac.in>
2020-03-28 16:02:42 +0530

    creating the TreeNode class.

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -31,4 +31,12 @@ public class TreeNode {
     public void setLeftChild(TreeNode leftChild) {
         this.leftChild = leftChild;
     }
+    public TreeNode getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(TreeNode rightChild) {
+        this.rightChild = rightChild;
+    }
+
 }
