482e372f6f4689efd2c09336a00beac268f2248e
Sanskar <sanskar.agrawal_ccv18@gla.ac.in>
2020-03-28 17:27:29 +0530

    Adding the student object in circular queue

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -5,6 +5,14 @@
  *  Time: 7:06 PM
  */
 package problem5.main;
+import problem5.circularqueue.MyCircularQueue;
+import problem5.student.Student;
 //executable class
 public class MyMain {
+    public static void main(String[] args) {
+        MyCircularQueue myCircularQueue = new MyCircularQueue(3);
+        myCircularQueue.enQueue(new Student("T", 0, 0));
+        myCircularQueue.enQueue(new Student("Y", 1, 1));
+        myCircularQueue.enQueue(new Student("D", 1, 0));
+    }
 }
