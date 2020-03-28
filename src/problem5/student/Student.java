7492640fba3199624d29514c4c6c67ac2428455b
Sanskar <sanskar.agrawal_ccv18@gla.ac.in>
2020-03-28 17:18:55 +0530

    Creating constructor

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -10,4 +10,10 @@ public class Student {
     private String name;
     private int backLog;
     private int appearingCount;
+    public Student(String name,int backLog,int appearingCount){
+        this.appearingCount=appearingCount;
+        this.backLog=backLog;
+        this.name = name;
+    }
+
 }
