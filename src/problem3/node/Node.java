c8ed193d59a546ed4a42be912e8d84fceecbfb77
Sanskar <sanskar.agrawal_ccv18@gla.ac.in>
2020-03-28 16:40:36 +0530

    Creating Node class

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -6,5 +6,42 @@
  */
 package problem3.node;
 
-public class Node {
+public class Node<T> {
+    private T data;
+    private Node next;
+    private int priority;
+
+    public Node(T data, int priority) {
+        this.data = data;
+        this.priority = priority;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    @Override
+    public String toString(){
+        return data.toString();
+    }
 }
