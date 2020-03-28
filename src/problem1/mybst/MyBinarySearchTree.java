08a462c38eb526202c210d13151ed378e98472bf
Sanskar <sanskar.agrawal_ccv18@gla.ac.in>
2020-03-28 16:09:17 +0530

    Creating insert method.

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -6,5 +6,50 @@
  */
 package problem1.mybst;
 // to implement BinarySearchTree
+import problem1.node.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
 public class MyBinarySearchTree {
+    private TreeNode root;
+
+    // To insert a new Node in Tree.
+    public void insert(int value){
+        TreeNode node = new TreeNode(value);
+        if(root == null){
+            root = node;
+            //System.out.println("root Node inserted");
+        }
+        else {
+            insertHelper(root,value);
+        }
+    }
+
+    private void insertHelper(TreeNode current,int value){
+        TreeNode node = new TreeNode(value);
+        if(value == current.getData()){
+            return;
+        }
+        else if(value < current.getData()){
+            if(current.getLeftChild() == null){
+                //System.out.println("New Node inserted");
+                current.setLeftChild(node);
+                node.setParent(current);
+            }
+            else {
+                insertHelper(current.getLeftChild(),value);
+            }
+        }
+        else {
+            if(current.getRightChild() == null){
+                //System.out.println("New Node inserted");
+                current.setRightChild(node);
+                node.setParent(current);
+            }
+            else {
+                insertHelper(current.getRightChild(),value);
+            }
+        }
+        return;
+    }
 }
