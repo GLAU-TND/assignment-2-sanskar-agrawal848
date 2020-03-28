1b41ad51ab430960946d209a72d6a5acaa056ba4
Sanskar <sanskar.agrawal_ccv18@gla.ac.in>
2020-03-28 16:56:02 +0530

    Creating object of Student Class.

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,16 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+import problem3.myqueue.MyPriorityQueue;
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        Student janeJones = new Student("Jane", "Jones", 1);
+        Student johnDoe = new Student("John", "Doe", 3);
+        Student marySmith = new Student("Mary", "Smith", 2);
+        Student mikeWilson = new Student("Mike", "Wilson", 4);
+
+        Student billEnd = new Student("Bill", "End", 5);
+    }
 }
