30f51c6cd6b743cbf5b61dc92621d40bad0b40d5
Sanskar <sanskar.agrawal_ccv18@gla.ac.in>
2020-03-28 17:22:10 +0530

    creating data elements and queue array

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -5,6 +5,13 @@
  *  Time: 7:06 PM
  */
 package problem5.circularqueue;
+import problem5.student.Student;
 //to implement circular queue
 public class MyCircularQueue {
+    private int size;
+    private int front;
+    private int rear;
+    private int maxSize;
+    private Student[] circularQueue;
+
 }
